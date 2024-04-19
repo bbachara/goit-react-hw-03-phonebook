b46328a7b6bd21f8613774c3e6d72af629cb2aa6
Bartek Bachara <bbachara@poczta.onet.pl>
2024-04-19 16:15:13 +0200

    react-hw-03-phonebook upd 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
@@ -6,14 +6,26 @@ import Filter from './Filter';
 import './styles.css';
 
 function App() {
-  const initialContacts = [
+  const initialContacts = JSON.parse(localStorage.getItem('contacts')) || [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ];
+
   const [contacts, setContacts] = useState(initialContacts);
+
+  useEffect(() => {
+    const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (storedContacts) {
+      setContacts(storedContacts);
+    }
+  }, []);
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = newContact => {
     if (contacts.some(contact => contact.name === newContact.name)) {
